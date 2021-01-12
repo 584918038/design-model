7ec777c43f105be3ce8c18573d820487dcd04fa6
xusisi <757507768@qq.com>
2021-01-12 14:58:50 +0800

    增减连接数据库的本领
    2021年01月12日14:58:49

---
+++ src/main/java/com/p6/demo/trasaction/Demo1.java
@@ -0,0 +1,21 @@
+package com.p6.demo.trasaction;
+
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author 徐恩晗 xshlxx@126.com
+ * @since 2021/1/11
+ */
+public class Demo1 {
+
+
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
+    public void test() {
+
+        // 首先明白一点，@Transactional默认帮我们开启了事务, 这个后面会说
+
+    }
+
+}
